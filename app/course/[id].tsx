1c40ecc52147b1486499a587e69ef88f3d394cef
Denis Matern <denismatern@gmail.com>
2025-01-20 12:50:57 +0300

    feature: dynamic route

---
+++ app/course/[id].tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../shared/tokens";
+import { useLocalSearchParams } from "expo-router";
+
+export default function CoursePage() {
+    const { id } = useLocalSearchParams();
+
+    return (
+        <View>
+            <Text style={styles.title}>
+                Страница курса №: {id}
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    title: {
+        color: Colors.white
+    }
+});
--- app/index.tsx
+++ app/index.tsx
@@ -30,6 +30,7 @@ export default function App() {
                     <Button title="Войти" onPress={alert} />
                 </View>
 
+                <CustomLink href="/course/1" text="Первый курс" />
                 <CustomLink href="/repassword" text="Восстановить пароль" />
             </View>
         </View>
