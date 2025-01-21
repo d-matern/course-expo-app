89464a111938c67749f74a891b43ee3dc699e92b
Denis Matern <denismatern@gmail.com>
2025-01-21 10:46:30 +0300

    feature: drawer

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,6 +1,8 @@
-import { Redirect, Stack } from "expo-router";
+import { Redirect } from "expo-router";
+import { Drawer } from "expo-router/drawer";
 import { useAtomValue } from "jotai";
 import { authAtom } from "../../entities/auth/model/auth.state";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function AppLayout() {
     const { accessToken } = useAtomValue(authAtom);
@@ -10,8 +12,10 @@ export default function AppLayout() {
     }
 
     return (
-        <Stack>
-            <Stack.Screen name="index" />
-        </Stack>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer>
+                <Drawer.Screen name="index" />
+            </Drawer>
+        </GestureHandlerRootView>
     );
 };
