6607137ffd2f457c5de58bd174d09e609589ad4a
Denis Matern <denismatern@gmail.com>
2025-02-03 13:15:46 +0300

    feature: pick avatar

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
-import { Alert, Text, View } from "react-native";
-import { launchCameraAsync, PermissionStatus, useCameraPermissions } from "expo-image-picker";
+import { Alert, Image, Text, View } from "react-native";
+import {
+    launchCameraAsync,
+    launchImageLibraryAsync,
+    PermissionStatus,
+    useCameraPermissions,
+    useMediaLibraryPermissions
+} from "expo-image-picker";
 import { Button } from "../../shared/Button";
 
 export default function Profile() {
     const [avatar, setAvatar] = useState<string | null>(null);
     const [cameraPermissions, requestCameraPermissions] = useCameraPermissions();
+    const [libraryPermissions, requestLibraryPermissions] = useMediaLibraryPermissions();
 
     const verifyCameraPermissions = async () => {
         if (cameraPermissions?.status === PermissionStatus.UNDETERMINED) {
@@ -19,9 +26,21 @@ export default function Profile() {
         return true;
     };
 
-    const pickAvatar = async () => {
-        const isVerifyCameraPermissions = await verifyCameraPermissions()
-        if (!isVerifyCameraPermissions) {
+    const verifyMediaPermissions = async () => {
+        if (libraryPermissions?.status === PermissionStatus.UNDETERMINED) {
+            const response = await requestLibraryPermissions();
+            return response.granted;
+        }
+        if (libraryPermissions?.status === PermissionStatus.DENIED) {
+            Alert.alert("Недостаточно прав для доступа к галерее");
+            return false;
+        }
+        return true;
+    };
+
+    const captureAvatar = async () => {
+        const isPermissionGranted = await verifyCameraPermissions()
+        if (!isPermissionGranted) {
             return;
         }
 
@@ -31,13 +50,39 @@ export default function Profile() {
             aspect: [1, 1],
             quality: 0.5
         });
-        console.log(result);
+
+        if (!result.assets) {
+            return;
+        }
+        setAvatar(result.assets[0].uri);
+    };
+
+    const pickAvatar = async () => {
+        const isPermissionGranted = await verifyMediaPermissions()
+        if (!isPermissionGranted) {
+            return;
+        }
+
+        const result = await launchImageLibraryAsync({
+            mediaTypes: 'images',
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 0.5
+        });
+
+        if (!result.assets) {
+            return;
+        }
+        setAvatar(result.assets[0].uri);
     };
 
     return (
         <View>
             <Text>Profile</Text>
-            <Button title="Изображение" onPress={pickAvatar} />
+            <Button title="Сделать фото" onPress={captureAvatar} />
+            <Button title="Выбрать из галереи" onPress={pickAvatar} />
+
+            {avatar && <Image source={{ uri: avatar }} width={100} height={100} />}
         </View>
     );
 }
